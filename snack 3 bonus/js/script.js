06751b45572dc35189ff2253de986b50ef5988e7
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-10 18:18:53 +0100

    feat: inizio bonus

--- snack 3 bonus/js/script.js
+++ snack 3 bonus/js/script.js
@@ -1,3 +1,17 @@
 // Snack 3 (Bonus)
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Non usare i metodi di array di JS 
+
+
+
+function getRandomNumber() {
+    let random = parseInt(Math.floor(Math.random() * 5) + 1);
+    return random;
+}
+
+
+let a = getRandomNumber();
+let b = a + getRandomNumber();
+
+
+
