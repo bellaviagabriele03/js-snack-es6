a4ac1ba6d2955c668b06c7fcc9d98056504e9e47
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-10 18:39:27 +0100

    feat: snack 3 completato

--- snack 3 bonus/js/script.js
+++ snack 3 bonus/js/script.js
@@ -2,16 +2,28 @@
 // Scrivere una funzione che accetti tre argomenti, un array e due numeri (a più piccolo di b). 
 // La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra i due numeri. Non usare i metodi di array di JS 
 
+let start = [];
 
+function getNewArray(array, a, b) {
+    if (a < b) {
+        for (let i = a; i < b; i++) {
+            start.push(`B è il più grande ${i}`)
+        }
 
-function getRandomNumber() {
-    let random = parseInt(Math.floor(Math.random() * 5) + 1);
-    return random;
+    } else if (a > b) {
+        for (let i = b; i < a; i++) {
+            start.push(`A è il più grande ${i}`)
+        }
+    } else if (a === b)
+        console.log("I numeri solo uguali!!!");
+    return start;
+    
+
 }
+let test1 = getNewArray(start, 10, 15)
+
+console.log(test1);
 
 
-let a = getRandomNumber();
-let b = a + getRandomNumber();
 
-
-
+// 
