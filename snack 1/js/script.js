8b7b7e8023d96f2a328b66ba58a32ef1d0a0a375
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-10 17:51:20 +0100

    fix: snack 1

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -7,33 +7,34 @@
 //array delle bici:
 const bici = [
     {
-        nome : "bmx",
-        peso : 20,
+        nome: "bmx",
+        peso: 20,
     },
     {
-        nome : "montain bike",
-        peso : 25,
+        nome: "montain bike",
+        peso: 25,
     },
     {
-        nome : "centurion",
-        peso : 30,
+        nome: "centurion",
+        peso: 30,
     },
     {
-        nome : "silver",
-        peso : 15,
+        nome: "silver",
+        peso: 15,
     }
 ]
 
 
-let result = "";
+let resultPeso = bici[0];
 
-for(let i = 0; i < bici.length; i++) {
-    let curIndex = bici[i]    
-    if(curIndex.peso < 20) {
-        result = curIndex.nome;
-    }
+
+for (let i = 0; i < bici.length; i++) {
+    
+    if(bici[i].peso < resultPeso.peso) {
+       resultPeso = bici[i].peso;
         
+    }
 }
 
-console.log(`La bici che pesa di meno è la: ${result}`);
+console.log(`La bici più leggera pesa: ${resultPeso} libre`);
 
--- snack 2/js/script.js
+++ snack 2/js/script.js
@@ -4,3 +4,41 @@
 // Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 // Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 // Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+
+// array delle squadre :
+const team = [
+    {
+        nome: "juventus",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "roma",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "David FC",
+        punti: 0,
+        falliSubiti: 0,
+    },
+    {
+        nome: "Riocuarto",
+        punti: 0,
+        falliSubiti: 0,
+    }
+]
+
+
+function getRandomFouls() {
+    let random = parseInt(Math.floor(Math.random() * 100) + 1);
+    return random;
+}
+
+console.log(getRandomFouls());
+
+
+function getRandomPoint() {
+    let random = parseInt(Math.floor(Math.random() * 30) + 1);
+}
