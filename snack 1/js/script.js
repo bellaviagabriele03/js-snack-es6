61d8566900f767fcf9ef4f2eb3c1fc46e92a4912
bellaviagabriele03 <bellaviagabriele03@gmail.com>
2025-11-10 17:15:31 +0100

    feat: snack 1 completato <3

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -4,3 +4,36 @@
 // Stampare a schermo oppure in console la bici con peso minore.
 
 
+//array delle bici:
+const bici = [
+    {
+        nome : "bmx",
+        peso : 20,
+    },
+    {
+        nome : "montain bike",
+        peso : 25,
+    },
+    {
+        nome : "centurion",
+        peso : 30,
+    },
+    {
+        nome : "silver",
+        peso : 15,
+    }
+]
+
+
+let result = "";
+
+for(let i = 0; i < bici.length; i++) {
+    let curIndex = bici[i]    
+    if(curIndex.peso < 20) {
+        result = curIndex.nome;
+    }
+        
+}
+
+console.log(`La bici che pesa di meno è la: ${result}`);
+
